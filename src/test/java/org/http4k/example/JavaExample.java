e8ebdcb5430d871ec597cff110cb2b7f38acb027
David Denton <denton.david@gmail.com>
2020-11-06 16:33:36 +0000

    reanimate java examples

--- src/test/java/org/http4k/example/JavaExample.java
+++ src/test/java/org/http4k/example/JavaExample.java
@@ -3,15 +3,18 @@ package org.http4k.example;
 import okhttp3.OkHttpClient;
 import org.http4k.client.DualSyncAsyncHttpHandler;
 import org.http4k.client.OkHttp;
-import org.http4k.core.*;
+import org.http4k.core.BodyMode;
+import org.http4k.core.Filter;
+import org.http4k.core.Method;
+import org.http4k.core.Request;
+import org.http4k.core.Response;
+import org.http4k.core.Status;
 import org.http4k.routing.RoutingHttpHandler;
+import org.http4k.routing.RoutingKt;
 import org.http4k.server.Http4kServer;
 import org.http4k.server.SunHttp;
 
 import static org.http4k.core.Http4kKt.then;
-import static org.http4k.core.Method.POST;
-import static org.http4k.routing.RoutingKt.bind;
-import static org.http4k.routing.RoutingKt.routes;
 import static org.http4k.server.Http4kServerKt.asServer;
 
 public class JavaExample {
@@ -19,20 +22,18 @@ public class JavaExample {
     public static void main(String[] args) {
         Filter f = next -> req -> next.invoke(req.body(req.bodyString().substring(0, 5)));
 
-        // TODO FIXME
+        RoutingHttpHandler routing =RoutingKt.bind("/path", Method.POST).to(req -> Response.Companion.create(Status.ACCEPTED));
 
-//        RoutingHttpHandler routing = routes(bind("/path", POST).to(req -> Response.Companion.create(Status.ACCEPTED)));
-//
-//        RoutingHttpHandler app = then(f, routing);
-//
-//        Http4kServer http4kServer = asServer(app, new SunHttp(8000));
-//
-//        http4kServer.start();
-//
-//        DualSyncAsyncHttpHandler client = OkHttp.create(new OkHttpClient.Builder().build(), BodyMode.Memory.INSTANCE);
-//
-//        System.out.println(client.invoke(Request.Companion.create(POST, "http://localhost:8000/path").body("1234567890")));
-//
-//        http4kServer.stop();
+        RoutingHttpHandler app = then(f, routing);
+
+        Http4kServer http4kServer = asServer(app, new SunHttp(8000));
+
+        http4kServer.start();
+
+        DualSyncAsyncHttpHandler client = OkHttp.create(new OkHttpClient.Builder().build(), BodyMode.Memory.INSTANCE);
+
+        System.out.println(client.invoke(Request.Companion.create(Method.POST, "http://localhost:8000/path").body("1234567890")));
+
+        http4kServer.stop();
     }
 }
